3729716964021ec00c8e3f4482cacf90264dde81
FarukDurakovic <farukdurakovic@gmail.com>
2017-04-17 20:02:03 -0400

    adding hyperlinks.. I hope!

--- unh698.py
+++ unh698.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "UNH698 Website"
+    return render_template('hello.html')
+	
+@app.route("/Topic")
+def howdy():
+    return render_template('howdy.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
--- unh698_test.py
+++ unh698_test.py
@@ -21,13 +21,13 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')  
         # Search the page contents for the link to your topic page 
         # Replace xxxxxxxxxxxx with text you'd expect to see on your main page that links to your subpage
-        assert b'Faruks Main Page' in rv.data 
+        assert b'Faruks Topic Page' in rv.data 
 		
     def test_my_topic(self):
         # Replace '/' with the page path you want to make
         rv = self.app.get('/Topic')  
         # Replace UNH698 Website with the text you expect to see on you topic page
-        assert b'Faruks Topic Page' in rv.data
+        assert b'awesome!' in rv.data
 		
 if __name__ == '__main__':
     unittest.main()
